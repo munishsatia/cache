da84dc85866d44b7cb2785d9c3f78b4286711b85
Munish Satia <msatia@reisystems.com>
2016-11-15 23:27:45 -0500

    redisCacheSample

---
+++ redisCacheSample/src/redisCacheSample/CacheResponsMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace redisCacheSample
+{
+    public class CacheResponsMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly IDistributedCache cache;
+        public CacheResponsMiddleware(RequestDelegate next, IDistributedCache cache)
+        {
+            this.next = next;
+            this.cache = cache;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            string cacheValue = "Nothing Found in Cache";
+            var val = await cache.GetAsync("CacheKey");
+            if (val != null)
+                cacheValue = Encoding.UTF8.GetString(val);
+
+            context.Response.Headers.Append("CacheValue", cacheValue);
+            await context.Response.WriteAsync(cacheValue + Environment.NewLine);
+            await next.Invoke(context);
+        }
+    }
+
+    public static class CacheResponseMiddlewareExtension
+    {
+        public static IApplicationBuilder UseCacheResponse(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CacheResponsMiddleware>();
+        }
+    }
+}
---
+++ redisCacheSample/src/redisCacheSample/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+
+namespace redisCacheSample
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDistributedRedisCache(option => 
+            {
+                option.Configuration = "localhost";
+                option.InstanceName = "redisCacheSample";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, 
+            IHostingEnvironment env, 
+            ILoggerFactory loggerFactory,
+            IDistributedCache cache)
+        {            
+            loggerFactory.AddConsole();
+            var cacheValue = "Munish : " + DateTime.Now.ToString();
+            var val = Encoding.UTF8.GetBytes(cacheValue);
+            cache.Set("CacheKey", val);
+
+            app.UseCacheResponse();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
